9798a00cb9810acbf6f52b77944711f105321983
rozttle <zzhang14@risd.edu>
2025-10-06 17:33:49 -0400

    toggle pages

---
+++ exercise6/toggle-script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const toggle = document.getElementById('toggleButton');
+  const toggleText = document.getElementById('toggle-text');
+
+  toggle.addEventListener('change', function() {
+    if (this.checked) {
+      console.log('Toggle is ON');
+      toggleText.textContent = 'Try again!';
+    } else {
+      console.log('Toggle is OFF');
+      toggleText.textContent = 'Yay! Nothing happened!'
+    }
+  });
+});
